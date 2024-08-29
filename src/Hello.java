398cd9e8aa5fe31e18a08f42d01ed0de556f7eea
setht <setht@s-obert.unl.edu>
2024-08-29 12:34:09 -0500

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * Author: Seth (sobert2@huskers.unl.edu) 
+ * Date: 8/29/2024
+ * 
+ * Prints my name
+ * 
+ * @author sobert
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Hello my name is Seth!");
+	}
+}
